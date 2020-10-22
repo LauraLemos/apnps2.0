1eab6d4ea210f042b74433d4935d80fcfeda6661
Laura Lemos <laurinhalemos78@gmail.com>
2020-10-22 14:57:28 -0300

    add rota /sobre

--- index.js
+++ index.js
@@ -5,10 +5,14 @@ app.get('/',function(req,res){
     res.send("Olá mundo, eu sou a Laura!!!")
     })
 
-    app.get('/msg',function(req,res){
-        res.send("OI, tudo bem?")
-        })
-  
+app.get('/msg',function(req,res){
+    res.send("OI, tudo bem?")
+         })
+
+ app.get('/sobre',function(req,res){
+      res.send("A pagina ainda está em construção")
+            })
+            
 app.listen(3000,function(){
 console.log("Conexão inicializada!!")
 })
