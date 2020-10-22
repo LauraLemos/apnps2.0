375f62bf46775761088154fa9186072667aebc25
Laura Lemos <laurinhalemos78@gmail.com>
2020-10-22 14:44:23 -0300

    add rota /mensagem

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const app = express()
 app.get('/',function(req,res){
     res.send("Olá mundo, eu sou a Laura!!!")
     })
+
+    app.get('/mensagem',function(req,res){
+        res.send("OI, tudo bem?")
+        })
   
 app.listen(3000,function(){
 console.log("Conexão inicializada!!")
