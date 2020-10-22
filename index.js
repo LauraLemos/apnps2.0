76b94697fd11e7055dcf8e9bf99631d1e99c72fa
Laura Lemos <laurinhalemos78@gmail.com>
2020-10-22 14:19:51 -0300

    commit do index 2.0

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+
+app.get('/',function(req,res){
+    res.send("Olá mundo, eu sou a Laura!!")
+    })
+  
+app.listen(3000,function(){
+console.log("Conexão inicializada")
+})
+
+
+
+
+
