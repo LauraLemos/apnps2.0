364773b152012fe8b68908141850b8aca57ba06a
Laura Lemos <laurinhalemos78@gmail.com>
2020-11-24 19:53:27 -0300

    aula 12 - add img c/ multer

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var bodyParser = require('body-parser')
 var cookieParser = require('cookie-parser')
 var path = require('path')
 var Usuario = require('./model/usuario')
+var upload = require('./config/configMulter')
 
 app.use(cookieParser())
 
@@ -15,80 +16,71 @@ app.set("view engine","ejs")
 app.use(express.static(path.join(__dirname,"public")))
 
 app.get('/',function(req,res){
-  Usuario.find({}).exec(function(err,docs){
-    res.render('index.ejs',{Usuarios: docs})
-  })
-   
- })
+    Usuario.find({}).exec(function(err,docs){
+        res.render('index.ejs',{Usuarios:docs})
+    })
+})
 
 app.post('/',function(req,res){
-  Usuario.find({nome: new RegExp(req.body.txtPesquisa, 'gi')}).exec(function(err,docs){
-    res.render('index.ejs',{Usuarios:docs})
-  })
+    Usuario.find({nome: new RegExp(req.body.txtPesquisa,'gi')}).exec(function(err,docs){
+        res.render('index.ejs',{Usuarios:docs})
+    })
 })
 
-
-
-
-app.get('/add', function(req,res){
-  res.render('adiciona.ejs')
+app.get('/add',function(req,res){
+    res.render('adiciona.ejs')
 })
 
-  app.post('/add',function(req,res){
+app.post('/add',upload.single("txtFoto"),function(req,res){
     var usuario = new Usuario({
-      nome: req.body.txtNome,
-      email: req.body.txtEmail,
-      senha: req.body.txtSenha,
-      foto: req.body.txtFoto
+        nome: req.body.txtNome,
+        email: req.body.txtEmail,
+        senha: req.body.txtSenha,
+        foto: req.file.filename
     })
     usuario.save(function(err){
-      if(err){
-          console.log(err)
-      }else{
-          res.redirect('/');
-      }
-    })
-  })
-
-  app.get('/del/:id', function(req,res){
-    Usuario.findByIdAndDelete(req.params.id, function(err){
-      if(err){
-        console.log(err)
-      }else{
-        res.redirect('/')
-      }
-    })
-  })
-
-    app.get('/edit/:id', function(req,res){
-      Usuario.findById(req.params.id,function(err,docs){
         if(err){
-          console.log(err)
-        }else{
-          res.render('edita.ejs',{Usuario: docs})
+            console.log(err)
+        }else {
+            res.redirect('/');
         }
-      })
     })
-
-app.post('/edit/:id',function(req,res){
-  Usuario.findByIdAndUpdate(req.params.id,
-    {nome:req.body.txtNome,
-     email:req.body.txtEmail, 
-     senha:req.body.txtSenha, 
-     foto:req.body.txtFoto
-    
-    },function(err,docs){
-      res.redirect('/')
-    }
-    )
 })
 
+app.get('/del/:id',function(req,res){
+    Usuario.findByIdAndDelete(req.params.id,function(err){
+        if(err){
+            console.log(err)
+        }else {
+            res.redirect('/')
+        }
+    })
+})
+
+app.get('/edit/:id',function(req,res){
+    Usuario.findById(req.params.id,function(err,docs){
+        if(err){
+            console.log(err)
+        }else{
+            res.render('edita.ejs',{Usuario: docs})
+        }
+    })
+    
+})
+
+app.post('/edit/:id',upload.single("txtFoto"), function(req,res){
+    Usuario.findByIdAndUpdate(req.params.id,
+        { 
+            nome: req.body.txtNome, 
+            email:req.body.txtEmail, 
+            senha:req.body.txtSenha, 
+            foto:req.file.filename 
+        },function(err,docs){
+            res.redirect('/')
+        }        
+        )
+})
 
 app.listen(3000,function(){
-console.log("Conexão inicializada!!")
+    console.log("Conexão inicializada")
 })
-
-
-
-
-
